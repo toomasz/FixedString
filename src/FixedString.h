73bcee6bc6e284dd07a86678bca4aa9b7c9c6ef8
Tomasz Scislowicz <toomasz@gmail.com>
2018-10-25 19:37:40 +0200

    reduce overhead to 5 bytes

--- src/FixedString.h
+++ src/FixedString.h
@@ -3,147 +3,117 @@
 
 #include <Arduino.h>
 #include <pgmspace.h>
-
-class FixedStringBase
-{
-public:
-	virtual const unsigned int length() const = 0;
-	virtual const unsigned int capacity() const = 0;
-
-	virtual const char *c_str() const = 0;
-	virtual bool append(const char *str, unsigned int len) = 0;
-	virtual bool append(char c) = 0;
-
-	virtual ~FixedStringBase() = default;
-	virtual void clear() = 0;
-};
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdint.h>
 
 extern bool FixedString_OverflowDetected;
 
-template <unsigned int N>
-class FixedString : public FixedStringBase
+#pragma pack(push, 1)
+
+class FixedStringBase
 {
 private:
-    char _string[N+1]{};
-	unsigned int _length;
+	uint16_t _length;
+	uint16_t _capacity;
 	bool AssertSpaceAvailable(int requestedLengthToAdd)
 	{
-	    if (_length + requestedLengthToAdd > N)
+		if (_length + requestedLengthToAdd > capacity())
 		{
 			FixedString_OverflowDetected = true;
 			return false;
 		}
 		return true;
 	}
+	char* _str() const
+	{
+		return (char*)this + sizeof(FixedStringBase);
+	}
 public:
-
-    FixedString()
-    {
-        clear();
-    }
-
-    FixedString(const char* str)
-    {
-        clear();
-        append(str);
-    }
-
-    const char* c_str() const
-    {
-        return _string;
-    }
-
-    const unsigned int length() const
-    {
-        return _length;
-    }
-
-    const unsigned int capacity() const
-    {
-        return N;
-    }
-
-    const unsigned int freeBytes() const
-    {
-        return N-length();
-    }
-
-	FixedString(FixedStringBase& str)
+	FixedStringBase(uint16_t capacity)
 	{
-        clear();
-        append(str.c_str(), str.length());
+		_capacity = capacity;
 	}
-	FixedString(const FixedString& str)
+	const char* c_str() const
 	{
-	    clear();
-        append(str._string, str._length);
+		return _str();
+	}
+	const unsigned int capacity()
+	{
+		return _capacity;
+	}
+	
+	const unsigned int length() const
+	{
+		return _length;
 	}
 
-	const FixedString<N> operator + (FixedStringBase& FixedString1)
+	const unsigned int freeBytes() const
 	{
-		FixedString<N> FixedString;
+		return _capacity - length();
+	}	
 
-		FixedString += *this;
-		FixedString +=FixedString1;
-
-		return  FixedString;
+	const char operator[](int index)
+	{
+		if (index < 0)
+		{
+			return 0;
+		}
+		if (index > length())
+		{
+			return 0;
+		}
+		return _str()[index];
 	}
 
-	FixedString<N> operator + (const char* str)
+	FixedStringBase& operator=(const char *str)
 	{
-		FixedString<N> FixedString;
-		FixedString +=*this;
-		FixedString += str;
-		
-		return FixedString;
+		clear();
+		append(str);
+		return *this;
+	}
+	
+	FixedStringBase& operator+=(const char *str)
+	{
+		append(str);
+		return *this;
+	}
+	FixedStringBase& operator =(const FixedStringBase& str)
+	{
+		clear();
+		FixedStringBase& strWithoutConst = const_cast<FixedStringBase&>(str);
+		append(const_cast<const char*>(strWithoutConst._str()), str.length());
+		return *this;
+	}
+	FixedStringBase& operator +=(FixedStringBase& str)
+	{
+		append(str._str(), str.length());
+		return *this;
+	}
+	
+	void clear()
+	{
+		_length = 0;
+		memset(_str(), 0, capacity() + 1);
 	}
 
-    FixedString<N>& operator=(const char *str)
-    {
-        clear();
-        append(str);
-        return *this;
-    }
-
-    FixedString<N>& operator+=(const char *str)
-    {
-        append(str);
-        return *this;
-    }
-    FixedString<N>& operator =(const FixedStringBase& str)
-    {
-        clear();
-        append(str.c_str(), str.length());
-        return *this;
-    }
-    FixedString<N>& operator +=(FixedStringBase& str)
-    {
-        append(str.c_str(), str.length());
-        return *this;
-    }
-
-    void clear()
-    {
-        _length = 0;
-        memset(_string, 0, N+1);
-    }
-
-    bool append(const char* str, unsigned int len)
-    {
-        if (!AssertSpaceAvailable(len))
+	bool append(const char* str, unsigned int len)
+	{
+		if (!AssertSpaceAvailable(len))
 		{
 			return false;
 		}
-        memcpy(_string + _length, str, len);
-        _length += len;
-        return true;
-    }
+		memcpy(_str() + _length, str, len);
+		_length += len;
+		return true;
+	}
 
-    bool append(const char* str)
-    {
-        const unsigned int len = strlen(str);
-        return append(str, len);
-    }
+	bool append(const char* str)
+	{
+		const unsigned int len = strlen(str);
+		return append(str, len);
+	}
 
 	bool append(char c)
 	{
@@ -151,19 +121,16 @@ public:
 		{
 			return false;
 		}
-		_string[_length] = c;
+		_str()[_length] = c;
 		_length++;
 		return true;
 	}
 
-	bool operator==(const char *str)
+	bool operator==(const char* str)
 	{
 		return equals(str);
 	}
-	bool operator==(const __FlashStringHelper *other)
-	{
-		return equals(other);
-	}
+
 	bool equals(const char* other)
 	{
 		auto otherLength = strlen(other);
@@ -171,7 +138,12 @@ public:
 		{
 			return false;
 		}
-		return memcmp(_string, other, _length) == 0;
+		return memcmp(c_str(), other, _length) == 0;
+	}
+#if !TEST
+	bool operator==(const __FlashStringHelper* other)
+	{
+		return equals(other);
 	}
 	bool equals(const __FlashStringHelper *other)
 	{
@@ -180,17 +152,7 @@ public:
 		{
 			return false;
 		}
-		return memcmp_P(_string, other, _length) == 0;
-	}
-
-	bool startsWith(const char* other)
-	{
-		auto otherLength = strlen(other);
-		if (otherLength > _length)
-		{
-			return false;
-		}
-		return memcmp(_string, other, otherLength) == 0;
+		return memcmp_P(c_str(), other, _length) == 0;
 	}
 	bool startsWith(const __FlashStringHelper* other)
 	{
@@ -199,30 +161,93 @@ public:
 		{
 			return false;
 		}
-		return memcmp_P(_string, other, otherLength) == 0;
+		return memcmp_P(c_str(), other, otherLength) == 0;
 	}
+#endif
+
+	bool startsWith(const char* other)
+	{
+		auto otherLength = strlen(other);
+		if (otherLength > _length)
+		{
+			return false;
+		}
+		return memcmp((char*)c_str(), other, otherLength) == 0;
+	}
+
 	void appendFormat(const char *format, ...)
 	{
-	    va_list argptr;
-        va_start(argptr, format);
+		va_list argptr;
+		va_start(argptr, format);
 		auto remainingSpace = freeBytes();
-	    auto addedCharacters = vsnprintf(_string + _length, remainingSpace, format, argptr);
+		auto addedCharacters = vsnprintf((char*)c_str() + _length, remainingSpace, format, argptr);
 		_length += addedCharacters;
 		va_end(argptr);
+	}
+	void appendFormat(const char *format, va_list argptr)
+	{		
+		auto remainingSpace = freeBytes();
+		auto addedCharacters = vsnprintf((char*)c_str() + _length, remainingSpace, format, argptr);
+		_length += addedCharacters;
 	}
 
 	void debug()
 	{
-        printf("content: '%s' length: %d, free = %d\n", c_str(), length(), freeBytes());
-        if(FixedString_OverflowDetected)
-        {
-            printf("Fatal error, string overun detected!\n");
-        }
+		printf("content: '%s' length: %d, free = %d\n", c_str(), length(), freeBytes());
+		if (FixedString_OverflowDetected)
+		{
+			printf("Fatal error, string overun detected!\n");
+		}
 	}
 };
 
+
+template <unsigned int N>
+class FixedString : public FixedStringBase
+{
+	char _string[N + 1]{};
+public:
+	const unsigned int capacity() const
+	{
+		return N;
+	}
+
+	FixedString():FixedStringBase(N)
+	{
+		clear();
+	}
+
+	FixedString(const char* str) :FixedStringBase(N)
+	{
+		clear();
+		append(str);
+	}
+	FixedString(FixedStringBase& str) :FixedStringBase(N)
+	{
+		clear();
+		append(str.c_str(), str.length());
+	}
+	FixedString(const FixedString& str) :FixedStringBase(N)
+	{
+		clear();
+		append(str.c_str(), str.length());
+	}
+
+	FixedString<N> operator + (FixedStringBase& FixedString1)
+	{
+		FixedString<N> FixedString(*this);
+		FixedString += FixedString1;
+		return FixedString;
+	}
+};
+typedef FixedString<10> FixedString10;
 typedef FixedString<20> FixedString20;
+typedef FixedString<50> FixedString50;
+typedef FixedString<100> FixedString100;
 typedef FixedString<150> FixedString150;
+typedef FixedString<200> FixedString200;
+
+#pragma pack(pop)
 
 #endif
 
